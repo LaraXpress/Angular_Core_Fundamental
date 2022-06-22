bc72f5f53c76cfd3002e6c2cf776fab0b7001f34
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-22 21:16:13 +0600

    ng-if directives with ng-template added

---
+++ Directives/Ng-IF/test.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit, NgModule } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  template: `
+    <h1 *ngIf="true">Welcome {{name}}</h1>    
+    <h3 *ngIf="displayName">Country Name : Bangladesh</h3>    
+    <hr>
+    <p *ngIf="contentBlock else elseBlock">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quibusdam, deserunt.</p>
+    <ng-template #elseBlock>
+      <p>Content is protected!</p>
+    </ng-template>
+    <hr>
+    <div *ngIf="language; then thenBlock else elseBlock"></div>
+    <ng-template #thenBlock>
+        <h2>Welcome to React Framework from  FACEBOOK</h2>
+    </ng-template>
+    <ng-template #elseBlock>
+        <h2>Welcome to Angular Framework from  Google & Angular</h2>
+    </ng-template>   
+  `,
+  styles: []
+})
+export class TestComponent implements OnInit {
+
+  public name : string = 'Angular Core Fundamental';      
+  public displayName : boolean = true;
+  public contentBlock : boolean = true;
+  public language = false;  
+  
+  constructor() { }
+  
+  ngOnInit(): void {
+  }  
+}
