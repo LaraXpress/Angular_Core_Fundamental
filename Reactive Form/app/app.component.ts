e81442293a1648a54b7e49704acb230f3f269f70
Mazedur (ISRT, DU) <89848156+LaraXpress@users.noreply.github.com>
2022-06-27 01:08:12 +0600

    reactive form created

---
+++ Reactive Form/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  constructor(){
+  }
+  title = 'HRM';  
+  registrationForm = new FormGroup({
+    username          : new FormControl('mazedur'),
+    email             : new FormControl(''),
+    password          : new FormControl(''),
+    c_password        : new FormControl('')
+  });
+}
+
